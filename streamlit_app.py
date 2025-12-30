1b6f5414c62e9292b656d81a8710c3ebd2e55e47
yig4001 <80707804+yig4001@users.noreply.github.com>
2025-12-30 12:30:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write("The name on your Smoothie will be: ", name_of_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 pd_df = my_dataframe.to_pandas()
@@ -33,13 +33,12 @@ if ingredients_list:
     for fruit in ingredients_list:
       ingredients_string += fruit + ' '
       
-      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
       #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
       st.subheader(fruit_chosen + ' Nutrition Information')
       smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/" + {search_on})
       sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
-
 
     st.write(ingredients_string)
 
